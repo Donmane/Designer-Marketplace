7b19e54760b37908721a123a3c7767058000a74f
Donmane <mdon85329@gmail.com>
2026-05-10 14:06:46 -0700

    Created Navbar.jsx and Hero.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+function App() {
+
+  return (
+    <>
+    <header>
+      <Navbar/>
+    </header>
+
+    <main>
+      <Hero/>
+    </main>
+    
+
+    </>
+  )
+}
+
+export default App
---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+    return(
+    <>
+    <nav>
+        <div>
+            <h1>Designora</h1>
+        </div>
+        <div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Designers</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </div>
+        <div>
+            <button>Continue as Client</button>
+            <button>Continue as Designer</button>
+        </div>
+    </nav>
+    </>
+    )
+}
+export default Navbar
