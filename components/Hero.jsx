004c9f8500c59f1ccf24542bec8b0ed43297150e
Donmane <mdon85329@gmail.com>
2026-05-12 02:11:48 -0700

    Created designer.json and FeaturedDesigner.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,10 @@
 function Hero() {
     return(
         <>
+        <div>
+            <h1>Hire creatives. <br /> Show your talent. <br/>Build your reputation </h1>
+            <p>Designora helps clients discover skilled <br />While giving creatives a place to showcase <br />their work</p>
+        </div>
         
         </>
     )
